911f2bbcf3feedd44b306bc6cbca0e1014631015
jodli <jobst87@gmail.com>
2020-02-19 13:54:36 +0100

    fold

--- Player.js
+++ Player.js
@@ -2,6 +2,34 @@ class Player {
   static get VERSION() {
     return 'Default C# folding player';
   }
+  static countCards(gameState) {
+    var cards = {
+      '2': 0,
+      '3': 0,
+      '4': 0,
+      '5': 0,
+      '6': 0,
+      '7': 0,
+      '8': 0,
+      '9': 0,
+      '10': 0,
+      'J': 0,
+      'Q': 0,
+      'K': 0,
+      'A': 0
+    };
+    var me = gameState["players"][gameState["in_action"]];
+    var hand = me["hole_cards"];
+
+    cards[hand[0]["rank"]] += 1;
+    cards[hand[1]["rank"]] += 1;
+
+    gameState["community_cards"].forEach(element => {
+      cards[element["rank"]] += 1;
+    });
+
+    return cards;
+  }
 
   static havePair(hand) {
     console.error("Check if pair: ");
@@ -9,14 +37,32 @@ class Player {
     console.error("hand[1]: " + hand[1]["rank"] + " " + hand[1]["suit"]);
     return hand[0]["rank"] == hand[1]["rank"];
   }
+  static checkCommunityCards(communityCards, hand) {
+    let current = ''
+    if (communityCards.length == 3) {
+      current = 'flop'
+    }
+  }
 
   static betRequest(gameState, bet) {
+    var cards = this.countCards(gameState);
     var me = gameState["players"][gameState["in_action"]];
+    var hand = me["hole_cards"];
+
+    if (!this.havePair(hand)) {
+      var number = Number(hand[0]["rank"]);
+      if ((number >= 2 && number <= 10)) {
+        var number = Number(hand[1]["rank"]);
+        if ((number >= 2 && number <= 10)) {
+          console.error("No high card.");
+          bet(0);
+          return;
+        }
+      }
+    }
 
     var check = gameState["current_buy_in"] - me["bet"];
     var raise = 0;
-
-    var hand = me["hole_cards"];
 
     if (this.havePair(hand)) {
       console.error("We have a pair.");
@@ -30,6 +76,7 @@ class Player {
       }
       console.error("Raise by: " + raise);
     }
+
 
     var bid = check + raise;
     bid = this.capRaise(bid, me["stack"]);
