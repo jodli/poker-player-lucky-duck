3777a029dd00bf9969ef5b67fc980a28611c298f
jodli <jobst87@gmail.com>
2020-02-19 10:03:51 +0100

    first push

--- Player.js
+++ Player.js
@@ -1,13 +1,17 @@
 class Player {
   static get VERSION() {
-    return '0.1';
+    return 'Go go Power Rangers!';
   }
 
   static betRequest(gameState, bet) {
-    bet(0);
+    var check = gameState["current_buy_in"] - gameState["players"][gameState["in_action"]]["bet"];
+    console.error("betRequest: " + check);
+    bet(check);
   }
 
   static showdown(gameState) {
+    console.error("showdown: ");
+    console.error(gameState);
   }
 }
 
